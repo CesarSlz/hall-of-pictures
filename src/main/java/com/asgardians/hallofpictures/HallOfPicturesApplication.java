59ccec27d3f27d595ea8b465cc43c99638ffbe3c
CesarSlz <cssalazar77@gmail.com>
2021-05-04 19:37:38 -0500

    Init commit

---
+++ src/main/java/com/asgardians/hallofpictures/HallOfPicturesApplication.java
@@ -0,0 +1,13 @@
+package com.asgardians.hallofpictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallOfPicturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallOfPicturesApplication.class, args);
+	}
+
+}
